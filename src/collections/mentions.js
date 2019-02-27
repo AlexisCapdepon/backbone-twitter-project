85d24ceadb8adb6361be9836f4460f24f2874dbe
Alexis capdepon <capdepon.alexis@gmail.com>
2019-02-27 21:18:09 +0100

    add comment

--- src/collections/mentions.js
+++ src/collections/mentions.js
@@ -11,17 +11,37 @@ var Mention = require('../models/mention.js');
 
 var Mentions = Backbone.Collection.extend({
   'model': Mention,
+  /**
+  * initialisation de la collection.
+  * @initialize
+  * @return {object} collection
+  */
   'initialize': function initialize () {
     var socket = io('http://localhost:8090');
 
-    socket.on('connection', function errorConnect (data) {
-      console.log(data);
-    });
+    /**
+    * connection to socket
+    * @connect
+    */
+    socket.on('connection', function connect () {
 
+    });
+    /**
+     * call the socket server.
+     * @loadTweets
+     * @param {object} data - new tweet.
+     * @return collection
+     */
     socket.on('tweets', function loadTweets (data) {
       this.addMention(data);
     }.bind(this));
   },
+  /**
+   * add mention to collection.
+   * @addMention
+   * @param {object} mention - new tweet to add.
+   * @return {this} collection
+   */
   'addMention': function addMention (mention) {
     if (this.length >= 6) {
       this.pop();
@@ -29,63 +49,7 @@ var Mentions = Backbone.Collection.extend({
       return;
     }
     this.unshift(mention);
-  },
-  'updateTweets': function updateTweets (item) {
-    this.set(item);
   }
 });
-
-// Api response
-// var response = [{
-//   'created_at': 'Tue Jan 15 10:22:02 +0000 2019',
-//   'id_str': '1085119914376077314',
-//   'text': 'We are hiring in the STATION F team! If you LOVE startups, with capital letters, and are looking for an internship,… https://t.co/1bqWJI5uDi',
-//   'user': {
-//     'name': 'STATION F',
-//     'profile_image_url': 'http://pbs.twimg.com/profile_images/937640813395431425/HfQbWe_1_normal.jpg'
-//   },
-//   'coordinates': null,
-//   'retweet_count': 2,
-//   'favorite_count': 12
-// }, {
-//   'created_at': 'Tue Jan 15 09:48:46 +0000 2019',
-//   'id_str': '1085111542725644288',
-//   'text': 'Réparer l\'ascenseur social, s\'engager à tous les étages, soutenir l\'égalité des chances ! Bravo @BNPParibas… https://t.co/QHg9s2jb0R',
-//   'user': {
-//     'name': 'La France s\'engage',
-//     'profile_image_url': 'http://pbs.twimg.com/profile_images/879939336212537345/9s_xtRrJ_normal.jpg'
-//   },
-//   'coordinates': null,
-//   'retweet_count': 4,
-//   'favorite_count': 8
-// }];
-
-// var formateur = function formateur (data) {
-//   var results = [];
-
-//   data.forEach(function loop (mention) {
-//     var item = {
-//       'createAt': moment(mention.created_at).fromNow(),
-//       'id': mention.id_str,
-//       'text': mention.text,
-//       'user': {
-//         'name': mention.user.name,
-//         'avatar': mention.user.profile_image_url
-//       },
-//       'replyCount': mention.reply_count || 0,
-//       'retweetCount': mention.retweet_count,
-//       'favoriteCount': mention.favorite_count,
-//       'coordonates': mention.coordonates
-//     };
-
-//     results.push(item);
-//   });
-//   return results;
-// };
-// var mentionsList = formateur(response);
-
-// mentionsList.forEach(function mentionLoop (item) {
-//   mentionsList.push(new Mention(item));
-// });
 
 module.exports = new Mentions();
--- src/controllers/twitter-realtime/components/tweets/index.js
+++ src/controllers/twitter-realtime/components/tweets/index.js
@@ -6,9 +6,18 @@ var tweetTpl = require('./tweet.tpl');
 module.exports = Backbone.View.extend({
   'el': '.mentions',
   'template': template(tweetTpl),
+  /**
+   * create the list of tweet
+   * @constructor
+   */
   'initialize': function initialize () {
     this.listenTo(this.collection, 'add', this.render);
   },
+  /**
+   * add the collection of tweet on the page.
+   * @render
+   * @return {this} the view
+   */
   'render': function render () {
     this.$el.html(this.template(this.collection.toJSON()));
     return this;
